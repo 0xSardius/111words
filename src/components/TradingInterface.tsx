d380d5d87c0830c948b9154f8d91e54864c7e128
Sardius <0xSardius@gmail.com>
2025-07-05 10:51:41 -0400

    fixed unused import error

--- src/components/TradingInterface.tsx
+++ src/components/TradingInterface.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState } from "react"
-import { useAccount } from "wagmi"
 import { useCoinTrading } from "../hooks/useCoinTrading"
 import { Button } from "./ui/Button"
 
